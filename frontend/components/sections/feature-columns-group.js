915607cb22b87a86290fef4cdda20bb9e4bb16c7
huynhtan1808 <tanhuynh1808@gmail.com>
2021-08-18 22:26:15 +0700

    Update

--- frontend/components/sections/feature-columns-group.js
+++ frontend/components/sections/feature-columns-group.js
@@ -2,10 +2,10 @@ import NextImage from "../elements/image"
 
 const FeatureColumnsGroup = ({ data }) => {
   return (
-    <div className="container flex flex-col lg:flex-row lg:flex-wrap gap-12 align-top py-12">
+    <div className="container grid md:grid-cols-3 gap-12 align-top py-12">
       {data.features.map((feature) => (
-        <div className="flex-1 text-lg" key={feature.id}>
-          <div className="w-10 h-10">
+        <div className="text-lg text-center py-5" key={feature.id}>
+          <div className="w-12 h-12 mx-auto">
             <NextImage media={feature.icon} />
           </div>
           <h3 className="font-bold mt-4 mb-4">{feature.title}</h3>
