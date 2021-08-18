191157ebcb725cc38d6577b2ce99bc86f7da2703
huynhtan1808 <tanhuynh1808@gmail.com>
2021-08-19 01:45:10 +0700

    Update sitemap

--- frontend/pages/sitemap.xml.js
+++ frontend/pages/sitemap.xml.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react"
 
-const BASE_URL = 'https://onepixelmedia.com/';
-const DATA_URL = process.env.DATA_URL || 'http://localhost:1337/pages';
+const BASE_URL = "https://onepixelmedia.com/"
+const DATA_URL = process.env.DATA_URL || "http://localhost:1337/pages"
 
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -13,21 +13,21 @@ const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
                         <lastmod>${updated_at}</lastmod>
                         <priority>1.0</priority>
                     </url>
-                `;
+                `
           })
-          .join('')}
+          .join("")}
     </urlset>
-    `;
+    `
 
 class Sitemap extends React.Component {
   static async getInitialProps({ res }) {
-    const request = await fetch(DATA_URL);
-    const posts = await request.json();
+    const request = await fetch(DATA_URL)
+    const posts = await request.json()
 
-    res.setHeader('Content-Type', 'text/xml');
-    res.write(createSitemap(posts));
-    res.end();
+    res.setHeader("Content-Type", "text/xml")
+    res.write(createSitemap(posts))
+    res.end()
   }
 }
 
-export default Sitemap;
+export default Sitemap
