3c19364563b6adef12c252d7df197acd9d366f24
huynhtan1808 <tanhuynh1808@gmail.com>
2021-08-18 16:17:06 +0700

    Init

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd395cba9cf05e6d996ac2f132251e991'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4df9d355-2eee-4e6a-8fca-69858d0636bf'
+};
