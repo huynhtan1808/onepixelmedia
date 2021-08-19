8da52062497e255a098a4970182019e3568231a2
huynhtan1808 <tanhuynh1808@gmail.com>
2021-08-19 21:03:29 +0700

    Update

--- frontend/components/sections/feature-rows-group.js
+++ frontend/components/sections/feature-rows-group.js
@@ -22,7 +22,7 @@ const FeatureRowsGroup = ({ data }) => {
           {/* Text section */}
           <div className="w-full lg:w-6/12 lg:pr-6 text-lg text-center">
             <h3 className="title">{feature.title}</h3>
-            <Markdown className="my-6 leading-loose text-gray-700 text-md">{feature.description}</Markdown>
+            <Markdown className="my-6 leading-loose text-md">{feature.description}</Markdown>
             <CustomLink link={feature.link}>
               <div className="text-blue-600 with-arrow hover:underline">{feature.link.text}</div>
             </CustomLink>
