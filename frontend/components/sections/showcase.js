2b2c5aa9b133b2a74ab242a7c3a7b9f4d317591c
huynhtan1808 <tanhuynh1808@gmail.com>
2021-08-20 16:36:24 +0700

    Update

--- frontend/components/sections/showcase.js
+++ frontend/components/sections/showcase.js
@@ -11,8 +11,8 @@ const Showcase = ({ data }) => {
             </figure>
           </div>
           <div className="opacity-0 group-hover:opacity-100 z-20 duration-300 text-white flex justify-center items-center">
-            <h3 className="text-xl font-semibold absolute mb-32">{project.title}</h3>
-            <a rel="nofollow noreferrer" href={project.description} target="_blank" className="absolute mb-20">
+            <h3 className="text-xl font-semibold absolute mb-40 md:mb-32">{project.title}</h3>
+            <a rel="nofollow noreferrer" href={project.description} target="_blank" className="absolute mb-28 md:mb-20">
               {project.description}
             </a>
           </div>
