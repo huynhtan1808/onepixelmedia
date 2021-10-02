fbb6aab0f2d5fe101191f985ddb1500ac2225f41
huynhtan1808 <tanhuynh1808@gmail.com>
2021-10-02 17:29:30 +0700

    Update

--- frontend/components/sections/head-banner.js
+++ frontend/components/sections/head-banner.js
@@ -2,10 +2,10 @@ import Image from "../elements/image"
 
 const HeadBanner = ({ data }) => {
   return (
-    <main className="bg-white pt-12 text-center text-primary">
+    <main className="bg-white pt-24 text-center text-primary">
       <div className="container max-w-5xl">
         <p className="uppercase tracking-wide font-semibold">{data.label}</p>
-        <h1 className="text-3xl md:text-5xl font-bold mt-2 mb-5">{data.title}</h1>
+        <h1 className="text-3xl md:text-5xl font-bold mt-2 mb-5 leading-normal">{data.title}</h1>
         <p className="text-lg md:text-xl">{data.description}</p>
       </div>
     </main>
